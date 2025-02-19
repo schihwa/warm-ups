ac3446b47053f817557e1a6ed4f1960a03dac41d
seth <schihwa109@gmail.com>
2025-02-19 16:16:49 +0000

    logical error detected 🎉

--- main.cpp
+++ main.cpp
@@ -9,20 +9,20 @@
 #include <string_view>
 #include <algorithm>
 #include <memory_resource>
-#include <new> 
+#include <new>
 #include <memory>
 
 // POD types don't need a destructor.
 struct TSTNode
 {
-    // TODO: RELATIVE OFFSETS for struct packing 
+    // TODO: RELATIVE OFFSETS for struct packing
     TSTNode *left = nullptr;
     TSTNode *middle = nullptr;
     TSTNode *right = nullptr;
     char character = '\0';
     bool isEndOfWord = false;
-    TSTNode() = default;     
-    // TODO: Add ctor value for middle variable      
+    TSTNode() = default;
+    // TODO: Add ctor value for middle variable
     explicit TSTNode(char ch) : character(ch) {}
 };
 
@@ -34,10 +34,10 @@ inline TSTNode *createNode(std::pmr::memory_resource *pool, char ch)
     return new_node;
 }
 
-// most words aren't really the same 
+// most words aren't really the same
 inline TSTNode *bulkCreateNodes(std::pmr::memory_resource *pool, std::string_view str)
 {
-    // 
+    //
     void *mem = pool->allocate(str.length() * sizeof(TSTNode), alignof(TSTNode));
     TSTNode *nodes = static_cast<TSTNode *>(mem);
 
@@ -52,45 +52,53 @@ inline TSTNode *bulkCreateNodes(std::pmr::memory_resource *pool, std::string_vie
 
 void insertWord(std::string_view word, TSTNode *&root, std::pmr::memory_resource *pool)
 {
-    //empty words are impossible. 
-        // yeah, every word does this dumb check. 
-        [[unlikely]]
-        if (root == nullptr)
+    // empty words are impossible.
+    //  yeah, every word does this dumb check.
+    [[unlikely]]
+    if (root == nullptr)
+    {
+        root = createNode(pool, word[0]);
+    }
+
+    TSTNode *currentNode = root;
+
+    // looks for the first occurrence of a unique branch
+    for (size_t index = 0; index < word.size(); ++index)
+    {
+        char currentChar = word[index];
+        TSTNode **childPtr = nullptr;
+
+        if (currentChar < currentNode->character)
         {
-            root = createNode(pool, word[0]);
+            childPtr = &currentNode->left;
+        }
+        else if (currentChar > currentNode->character)
+        {
+            childPtr = &currentNode->right;
+        }
+        else
+        {
+            childPtr = &currentNode->middle;
         }
 
-        TSTNode *currentNode = root;
+        if (*childPtr == nullptr)
+        {
 
-        // looks for the first occurrence of a unique branch
-        for (size_t index = 0; index < word.size(); ++index) {
-            char currentChar = word[index];
-            TSTNode** childPtr = nullptr;
-            
-            if (currentChar < currentNode->character) {
-                childPtr = &currentNode->left;
-            } else if (currentChar > currentNode->character) {
-                childPtr = &currentNode->right;
-            } else {
-                childPtr = &currentNode->middle;
-            }
-            
-            if (*childPtr == nullptr) {
-                *childPtr = bulkCreateNodes(pool, word.substr(index));
-                currentNode = *childPtr;
-                break; 
-            }
-            
+            // FIX: When the loop breaks isEndOfWord becomes the first bulk node.
+            *childPtr = bulkCreateNodes(pool, word.substr(index));
             currentNode = *childPtr;
+            break;
         }
-        
-        currentNode->isEndOfWord = true;
+
+        currentNode = *childPtr;
+    }
+    currentNode->isEndOfWord = true;
 }
 
 // TODO: user input with listeners on the typing
 int main()
 {
-    std::pmr::monotonic_buffer_resource pool; 
+    std::pmr::monotonic_buffer_resource pool;
     TSTNode *root = nullptr;
 
     std::string word1 = "HELLO";
@@ -98,8 +106,6 @@ int main()
 
     insertWord(word1, root, &pool);
     insertWord(word2, root, &pool);
-
-
 
     return 0;
 }
